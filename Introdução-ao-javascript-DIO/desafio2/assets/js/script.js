ee381037175e35faa3e2fc07e56b97f82043b97d
laurourbano <74082012+laurourbano@users.noreply.github.com>
2022-04-26 21:48:44 -0300

    1

--- Introdução-ao-javascript-DIO/desafio2/assets/js/script.js
+++ Introdução-ao-javascript-DIO/desafio2/assets/js/script.js
@@ -1,2 +1,41 @@
-var mensagemDeCarregamento = document.querySelector(".rodape");
-mensagemDeCarregamento.textContent = "javascript do documento carregado com sucesso";
+/*var mensagemDeCarregamento = document.querySelector(".rodape");
+mensagemDeCarregamento.textContent = "javascript do documento carregado com sucesso";*/
+
+function comparaNumeros(numero1, numero2){
+    const primeiraFrase = criaPrimeiraFrase(numero1, numero2);
+    const segundaFrase = criaSegundaFrase(numero1, numero2);
+
+    return `${primeiraFrase} ${segundaFrase}`
+}
+
+function criaPrimeiraFrase(numero1, numero2){
+    let saoIguais = '';
+
+    if(numero1 !== numero2){
+        saoIguais = 'não';
+    }
+
+    return `Os números ${numero1} e ${numero2} ${saoIguais} são iguais.`
+}
+
+function criaSegundaFrase(numero1, numero2){
+    const soma = numero1 + numero2;
+    
+    let resultado10 = 'menor';
+    let resultado20 = 'menor';
+    const compara10 = soma > 10;
+    const compara20 = soma > 20;
+
+
+    if(compara10){
+        resultado = 'maior';
+    }
+
+    if(compara20){
+        resultado20 = 'maior';
+    }
+
+    return `Sua soma é ${soma} que é ${resultado10} que 10 e ${resultado20} que 20.`
+}
+
+console,log(comparaNumeros(1,2));
