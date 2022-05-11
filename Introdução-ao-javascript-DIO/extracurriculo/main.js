66da4f1b766a554094d3c6ae3dac9f760575aa59
laurourbano <74082012+laurourbano@users.noreply.github.com>
2022-05-10 22:18:52 -0300

    jão

--- Introdução-ao-javascript-DIO/extracurriculo/main.js
+++ Introdução-ao-javascript-DIO/extracurriculo/main.js
@@ -1,9 +1,13 @@
 const corpo = document.querySelector(".dark-mode");
-const botao = document.querySelector(".inverter")
+const botao = document.querySelector(".botao");
 
-botao.addEventListener("click", function(){
+function mudaTema(){
         corpo.style.backgroundColor = "black";
         corpo.style.color = "white";
         botao.style.backgroundColor = "white";
         botao.style.color = "black";
-});
+};
+
+function mudaTexto(id){
+        id.innerHTML = "Mudar para Tema Claro";
+};
